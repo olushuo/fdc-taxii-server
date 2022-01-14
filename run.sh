2193d97db37e85e1c308299233a1c05c20e975fb
olushuo <iamlushu@gmail.com>
2022-01-13 18:30:26 -0800

    Setup a TAXII server for testing FDC STIX feature

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker image pull olushuo/fileserver:1.0
-docker container run --name taiiserver -d -p 80:80 olushuo/fdc-taxii-server:1.0
+sudo docker image pull olushuo/fdc-taxii-server:1.0
+sudo docker container run --name taiiserver -d -p 9001:9001 olushuo/fdc-taxii-server:1.0
